681915d4eee545b0251569e5bfaba599ac77c1b5
SIDERODROMOPHOBE <salibaa.gabriel@gmail.com>
2023-11-22 17:33:00 +0100

    Completed deploy script

--- Contracts/script/Counter.s.sol
+++ Contracts/script/Counter.s.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 
-import {Vote} from "../src/Vote.sol";
-
+import {Sondage} from "../src/Vote.sol";
 
 contract DeploymentScript is Script {
     function run() external {
@@ -12,7 +11,7 @@ contract DeploymentScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_ANVIL");
         vm.startBroadcast(deployerPrivateKey);
 
-        new Vote();
+        new Sondage();
         
 
         vm.stopBroadcast();
