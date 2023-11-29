f7ecaecd80a2a920cd38818c26a1c6f2eaab3d8f
SIDERODROMOPHOBE <salibaa.gabriel@gmail.com>
2023-11-29 16:36:56 +0100

    Created Write functions hooks

--- src/components/Tro.tsx
+++ src/components/Tro.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import "Styles/globals.css";
 import {abi} from "./contracts"
 
-import { useContractRead } from 'wagmi'
+import { useContractRead, useContractWrite,usePrepareContractWrite  } from 'wagmi'
 import { readContract } from "viem/dist/types/actions/public/readContract";
 
 
@@ -14,21 +14,33 @@ export default function Tro()
     const [a,seta] = useState(true);
     const [openContract,setOpenContract] = useState(1);
 
+    const [newSondageName,setNewSondageName] = useState("");
+    const [newSondageOP1,setNewSondageOP1] = useState("");
+    const [newSondageOP2,setNewSondageOP2] = useState("");
 
-    const Sondage_count = useContractRead(
+    const [chosenSondage,setChosenSondage]=useState(1);
+    const [chosenVote,setChosenVote]=useState(1);
+
+
+    const updateSondageName = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewSondageName(e.target.value);
+    };
+    const updateSondageOP1 = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewSondageOP1(e.target.value);
+    };
+    const updateSondageOP2 = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewSondageOP2(e.target.value);
+    };
+
+
+
+    const sondageCountq = useContractRead(
         {
             address: '0x721AB533c5f1F94CE0e4728e43E21d69e5d46D56',
             ...abi,
             functionName:'Sondage_count',
-            
-            
         })
-        var Sondages_count = Number(Sondage_count.data);
-        
-
-
-        
-
+    const sondageCount = Number(sondageCountq.data);
            
     const sondageInfo = useContractRead(
         {
@@ -40,43 +52,93 @@ export default function Tro()
                             
         })
             
-        var sondageData=sondageInfo.data?.toString().split(',');
-                    
-            
-                    
-            
+    var sondageData=sondageInfo.data?.toString().split(',');
     
-                
+    
+        
+    const prepareSondageWrite = usePrepareContractWrite(
+        {
+            address: '0x721AB533c5f1F94CE0e4728e43E21d69e5d46D56',
+            ...abi,
+            functionName: 'creerSondage',
+            args:[newSondageName,newSondageOP1,newSondageOP2],
+        })
+    
+    const sondageWrite = useContractWrite(prepareSondageWrite.config);
     
 
     
+    const prepareVoteWrite = usePrepareContractWrite(
+        {
+            address: '0x721AB533c5f1F94CE0e4728e43E21d69e5d46D56',
+            ...abi,
+            functionName: 'voter',
+            // @ts-ignore
+            args:[chosenSondage,chosenVote],
+        })
+        
+        const voteWrite = useContractWrite(prepareVoteWrite.config);
 
-    function write()
+    function wriite()
     {
         seta(!a);
-        console.log(Sondages_count);
+        // @ts-ignore
+        console.log(typeof sondageCount)
     }
 
-    
-    
+    function toggleFormNewSondage() 
+    {
+        setFormNewSondage(!formNewSondage);
+        setNewSondageName("");
+        setNewSondageOP1("");
+        setNewSondageOP2("");
+    }
+    const [formNewSondage,setFormNewSondage] = useState(false);
+
+
+
     return (
-        <>
+        <>        
+        {!formNewSondage &&<button onClick={toggleFormNewSondage}>Créer un nouveau sondage</button>}
+        
+        {formNewSondage && 
+        <div>
+        <label>Nom du nouveau sondage</label>
+            <input placeholder="Sondage Name" type="text" onChange={updateSondageName} value ={newSondageName}></input>
+            &nbsp;
+            <label>Nom de l'option de vote 1</label>
+            <input placeholder="Vote Option" type="text" onChange={updateSondageOP1} value ={newSondageOP1}></input>
+            &nbsp;
+            <label>Nom de l'option de vote 2</label>
+            <input placeholder="Other vote option" type="text" onChange={updateSondageOP2} value ={newSondageOP2}></input>
+            <br/>
 
 
 
-        <button onClick={write}>DEBUG</button> <br/>
+            <button onClick={() => sondageWrite.write?.()}>Créer le Sondage</button>
+<br/>
+            <button onClick={toggleFormNewSondage}>Quitter</button>
+        </div>
+        }
+
+
+
+        <br/>
+        <button onClick={wriite}>DEBUG</button> <br/>
         {sondageData}
 
         {a && 
 
             <div>
-                <h1>il y a {Sondage_count.data?.toString()} sondages</h1>
+                <h1>il y a {// @ts-ignore 
+                sondageData[0]} sondages</h1>
                 <br></br>
-                <h1>Opened poll number : {openContract}</h1>
-                <h2 className="px-5 underline ">{}</h2>
+                <h1>Réferendum numéro  {openContract} :</h1>
+                
 
-                    <div className="box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-5">
-                        Piou piou
+                    <div className="underline box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-5">
+                        {// @ts-ignore
+                        sondageData[1]}
                     </div>
 
             </div>
