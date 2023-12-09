b95045438eae1e1df70c386042f323d3dfe5c870
SIDERODROMOPHOBE <salibaa.gabriel@gmail.com>
2023-12-09 19:33:44 +0100

    Added visualisation des résultats

--- src/components/Tro.tsx
+++ src/components/Tro.tsx
@@ -251,6 +251,16 @@ return (
                 </div>
     </center>
 
+<br/>
+
+<div className="m-5 border border-2 border-white">
+    <div className="m-2">
+    <h1>Résultats du sondage :</h1>
+
+    <h2>Votes pour {(typeof sondageData !== 'undefined')?sondageData[2]:""} : {(typeof sondageData !== 'undefined')?sondageData[5]:""} </h2>
+    <h2>Votes pour {(typeof sondageData !== 'undefined')?sondageData[3]:""} : {(typeof sondageData !== 'undefined')?sondageData[4]:""} </h2>
+    </div>
+</div>
 
 
     {!formNewSondage &&
